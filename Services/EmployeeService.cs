a0b742bd8ea69c1850917484e2900463e791fb84
Shadman Khan <Shadmankhansanir@gmail.com>
2021-05-17 16:11:04 +0600

    added employee service class

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeModule.Services
+{
+    public class EmployeeService
+    {
+    }
+}
